a8259229f58ab30a91a4d9424616d5e076e254d7
Magnus Axelson-Fisk <magnus.axelson.fisk@gmail.com>
2015-07-28 19:33:48 +0200

    Fixed problems with y-start when cropping

--- imageCrop.py
+++ imageCrop.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 # Magnus Axelson-Fisk
+# Hopefully this will work later without pageWidth or pageHeigth
+# but instead with imagemagick canny function
 # Bokskanner 2015
 
 import PythonMagick as PM
 
 
 def imageCrop(pageWidth, pageHeight, bookLength):
-    pixPerCm = 14
+    pixPerCm = 98.4 #update when measured 3888/picturelength
     pageID = 0
     
     pageWidthPix = pixPerCm*pageWidth*2
-    pageHeigthPix = pixPerCm*pageHeigth
+    pageHeightPix = pixPerCm*pageHeight
     
     cropXPixel = 1944-(pageWidth*pixPerCm)
-    cropArea = "%sx%s+%s+%s" % (pageWidthPix, pageHeigthPix, cropXPixel, pageHeigthPix)
+    cropYPixel = 2592-pageHeightPix
+    cropArea = "%sx%s+%s+%s" % (pageWidthPix, pageHeightPix, cropXPixel, cropYPixel)
 
     
     for pageID in range(0,bookLength):
         img = PM.Image(str(pageID) + ".jpg")
         img.rotate(180.0)
         img.crop(cropArea)
-        img.write(pageID + "crop.jpg")
+        img.write(str(pageID) + "crop.jpg")
     if pageID == (bookLength - 1):
         return True
     else:
