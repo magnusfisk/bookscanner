4e9b37c216c05d81fa9683e2c5ec09584315cc33
Magnus Axelson-Fisk <magnus.axelson.fisk@gmail.com>
2015-07-28 12:39:23 +0200

    started on imagecroping

---
+++ imageCrop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Magnus Axelson-Fisk
+# Bokskanner 2015
+
+import PythonMagick as PM
+
+def imageCrop(pageWidth, pageHeight, bookLength):
+    pixPerCm = 14
+    pageID = 0
+    
+    pageWidthPix = pixPerCm*pageWidth*2
+    pageHeigthPix = pixPerCm*pageHeigth
+    
+    cropXPixel = 1944-(pageWidth*pixPerCm)
+    cropArea = "%sx%s+%s+%s" % (pageWidthPix, pageHeigthPix, cropXPixel, pageHeigthPix)
+
+    
+    for pageID in range(0,bookLength):
+        img = PM.Image(str(pageID + ".jpg")
+        img.rotate(180)
+        img.crop(cropArea)
+        
+
+    
